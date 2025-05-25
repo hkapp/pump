9a943a81510bece4156cdb4d448d362dd5e28323
Hugo Kapp <hugo.kapp6@hotmail.fr>
2025-05-25 10:30:42 +0200

    Take a Captures as argument instead of Match when building tokens

--- src/parse/token.rs
+++ src/parse/token.rs
@@ -4,32 +4,10 @@ use regex::Regex;
 
 use crate::error::Error;
 
-#[derive(Debug)]
-pub struct Identifier {
-    pub name:     String,
-    pub position: ParsePos
-}
-
-impl Identifier {
-    fn token(m: &regex::Match) -> Token {
-        let pos = ParsePos::from(m);
-        let idn =
-            Identifier {
-                name:     m.as_str().into(),
-                position: pos,
-            };
-        Token { position: pos, kind: Kind::Identifier(idn) }
-    }
-
-    /// Take ownership of an identifier behind a ref mut,
-    /// leaving the ref pointed Identifier in a Rust-valid but
-    /// semantically invalid state.
-    pub fn take(&mut self) -> Self {
-        // Note: the docs tell us that String::new() does not lead to an allocation
-        let name = std::mem::replace(&mut self.name, String::new());
-        let position = self.position;
-        Self { name, position }
-    }
+// Should we get rid of the position field and implement the Position trait?
+pub struct Token {
+    pub position: ParsePos,
+    pub kind:     Kind,
 }
 
 pub fn tokenize<'a>(s: &'a str) -> Tokenizer<'a> {
@@ -44,11 +22,6 @@ pub struct Tokenizer<'a> {
     source:   &'a str,
     curr_pos: usize,
     token_rxs:  Vec<TokenRx>,
-}
-
-pub struct Token {
-    pub position: ParsePos,
-    pub kind:     Kind,
 }
 
 pub enum Kind {
@@ -130,7 +103,7 @@ impl<'a> Tokenizer<'a> {
 
 // Note: this definition forbids using the From trait implementation
 // TODO we can turn this into a function that returns a Kind
-type BuildFn = fn(&regex::Match) -> Token;
+type BuildFn = fn(&regex::Captures) -> Token;
 
 struct TokenRx {
     regex:    Regex,
@@ -148,29 +121,58 @@ impl From<&TRDef> for TokenRx {
     }
 }
 
-fn regex_match(m: &regex::Match) -> Token {
-    // FIXME we need to get a regex::Captures instead as argument
-    let regex_substr = &m.as_str()[2..(m.len()-1)];
+const TOKEN_RXS: [TRDef; 2] = [
+    // WARNING the ordering matters here
+    ("m/((?:[^/]|\\/)*)/",     regex_match),
+    ("[a-zA-Z][0-9a-zA-Z]*", Identifier::token),
+];
+
+fn regex_match(rec: &regex::Captures) -> Token {
+    let m = rec.get(1).unwrap();
+    let regex_substr = m.as_str();
     // FIXME need to return a proper error here
+    // What are the cases when this can fail though? Unmatched parentheses maybe?
     let re_match = Regex::new(regex_substr).unwrap();
 
-    let pos = ParsePos::from(m);
+    let pos = ParsePos::from_captures(rec);
     Token { position: pos, kind: Kind::RegexMatch(re_match) }
 }
 
-const TOKEN_RXS: [TRDef; 2] = [
-    // WARNING the ordering matters here
-    // ("m/(?:[^/]|\\/)*/\\>", regex_match),
-    ("m/(?:[^/]|\\/)*/", regex_match),
-    ("[a-zA-Z][0-9a-zA-Z]*", Identifier::token),
-];
+#[derive(Debug)]
+pub struct Identifier {
+    pub name:     String,
+    pub position: ParsePos
+}
+
+impl Identifier {
+    fn token(rec: &regex::Captures) -> Token {
+        let m = rec.get(0).unwrap();
+        let pos = ParsePos::from_match(&m);
+        let idn =
+            Identifier {
+                name:     m.as_str().into(),
+                position: pos,
+            };
+        Token { position: pos, kind: Kind::Identifier(idn) }
+    }
+
+    /// Take ownership of an identifier behind a ref mut,
+    /// leaving the ref pointed Identifier in a Rust-valid but
+    /// semantically invalid state.
+    pub fn take(&mut self) -> Self {
+        // Note: the docs tell us that String::new() does not lead to an allocation
+        let name = std::mem::replace(&mut self.name, String::new());
+        let position = self.position;
+        Self { name, position }
+    }
+}
 
 impl TokenRx {
     fn try_at(&self, source: &str, start: usize) -> Option<Token> {
         self.regex
-            .find_at(source, start)
-            .filter(|m| m.start() == start)
-            .map(|m| (self.build_fn)(&m))
+            .captures_at(source, start)
+            .filter(|rec| rec.get(0).unwrap().start() == start)
+            .map(|rec| (self.build_fn)(&rec))
     }
 }
 
@@ -185,11 +187,17 @@ impl ParsePos {
         ParsePos { start: pos, len: 1 }
     }
 
-    fn from(m: &regex::Match) -> Self {
+    fn from_match(m: &regex::Match) -> Self {
         ParsePos {
             start: m.start(),
             len:   m.as_str().len()
         }
+    }
+
+    fn from_captures(rec: &regex::Captures) -> Self {
+        // Note: the documentation for Captures guarantees that
+        // get(0) will never return None
+        Self::from_match(&rec.get(0).unwrap())
     }
 
     pub fn right_after(&self) -> ParsePos {
